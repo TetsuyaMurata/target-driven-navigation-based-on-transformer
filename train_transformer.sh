e811490763f5a661ac0fa58598335349a481396c
TetsuyaMurata <73677170+TetsuyaMurata@users.noreply.github.com>
2022-06-02 10:26:06 +0900

    Update train_transformer.sh

--- train_transformer.sh
+++ train_transformer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source /etc/profile.d/modules.sh
-#export SINGULARITY_BINDPATH=/groups/gac50631/Rui/target-driven-navigation-based-on-transformer
 #export SINGULARITY_BINDPATH=/groups/gac50631
-#export SINGULARITY_BINDPATH=/groups/gac50631/dataset/visual-navigation-agent-pytorch/h5_files_8action_45degree/data
-#export SINGULARITY_BINDPATH=/groups/gac50631/dataset/visual-navigation-agent-pytorch/h5_files_25cm_8action_45degree/data
 #module load singularitypro
 #module load cuda/10.2/10.2.89
 #module load cudnn/8.0/8.0.5
