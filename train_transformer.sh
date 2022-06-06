d549abe3571cd89c30a6a4f01af546176a00ab22
TetsuyaMurata <73677170+TetsuyaMurata@users.noreply.github.com>
2022-06-06 10:18:48 +0900

    Update train_transformer.sh

--- train_transformer.sh
+++ train_transformer.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-source /etc/profile.d/modules.sh
-#export SINGULARITY_BINDPATH=/groups/gac50631
-#module load singularitypro
-#module load cuda/10.2/10.2.89
-#module load cudnn/8.0/8.0.5
-#cd /home/dl-box/target-driven-navigation-based-on-transformer
-#source activate env_ai
+source /etc/profile.d/modules.sh #abci
+#export SINGULARITY_BINDPATH=/groups/gac50631 #abci
+#module load singularitypro #abci
+#module load cuda/10.2/10.2.89 #abci
+#module load cudnn/8.0/8.0.5 #abci
+#cd target-driven-navigation-based-on-transformer
 python memory_size.py #memory_size can be changed
-#singularity exec --nv /home/dl-box/target-driven-navigation-based-on-transformer/cenotate_transformer.simg python -u train.py #origin
-python -u train.py #origin
+#singularity exec --nv /home/dl-box/target-driven-navigation-based-on-transformer/cenotate_transformer.simg python -u train.py #abci
+python -u train.py #local
