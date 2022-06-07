7ed0e10aac00fc6ab4fb97ae794ed9b9a9cdf191
TetsuyaMurata <73677170+TetsuyaMurata@users.noreply.github.com>
2022-06-07 15:22:11 +0900

    Update eval_transformer_abci.sh

--- eval_transformer_abci.sh
+++ eval_transformer_abci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /etc/profile.d/modules.sh
-export SINGULARITY_BINDPATH=/groups/gac50631 #Used on occasion
+#export SINGULARITY_BINDPATH=/groups/gac50631 #Used on occasion
 module load singularitypro
 module load cuda/10.2/10.2.89
 module load cudnn/8.0/8.0.5
