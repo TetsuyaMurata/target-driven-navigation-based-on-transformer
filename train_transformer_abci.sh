ad89aeb76d136fd384f9d2b403e4d3f633f69f1f
cenotence <rui.rhythem@gmail.com>
2022-06-10 16:15:24 +0900

    update bash file

--- train_transformer_abci.sh
+++ train_transformer_abci.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-source /etc/profile.d/modules.sh
-#export SINGULARITY_BINDPATH=/groups/gac50631 #Used on occasion
-module load singularitypro
-module load cuda/10.2/10.2.89
-module load cudnn/8.0/8.0.5
+
+#$ -l rt_F=1
+#$ -l h_rt=20:00:00
+#$ -j y
+#$ -m abe
+
+source /etc/profile.d/modules.sh #abci
+
+module load cuda/10.2/10.2.89 #abci
+module load cudnn/8.0/8.0.5 #abci
+cd Code/target-driven-navigation-based-on-transformer
+
+source .vna/bin/activate
 python memory_size.py #memory_size can be changed
-singularity exec --nv cenotate_transformer.simg python -u train.py
+#singularity exec --nv cenotate_transformer.simg python -u train.py #abci
+python -u train.py #local
