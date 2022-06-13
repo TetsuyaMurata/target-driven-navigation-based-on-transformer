2099c33f5063eee78fcfa5306d64cd05b15d6012
TetsuyaMurata <73677170+TetsuyaMurata@users.noreply.github.com>
2022-06-13 13:08:19 +0900

    Update train.py

--- train.py
+++ train.py
@@ -5,12 +5,10 @@ import multiprocessing as mp
 
 from agent.training import Training
 from agent.utils import populate_config
+from pathlib import Path
 
-# target_path = './model/Transformer_word2vec/80scene/45deg/1layer/grid_memory/50cm/seed/4hist/61/concat' #add
-# target_path = '/home/dl-box/target-driven-navigation-based-on-transformer/model/Transformer_word2vec/80scene/45deg/1layer/grid_memory/50cm/seed/4hist/61' #add
-# target_path = '/home/dl-box/target-driven-navigation-based-on-transformer/model/Transformer_word2vec/80scene/45deg/1layer/grid_memory/50cm/seed/32hist/61' #add
-# with open('target_path.txt', mode='r', encoding='utf-8') as f:
-    # target_path = f.readline().replace('\n', '')
+Path("tmp").mkdir(exist_ok=True)
+
 with open('.env', mode='r', encoding='utf-8') as f:
     target_path = "EXPERIMENT/" + f.readline().replace('\n', '')
 print("TARGET : {}".format(target_path.replace("EXPERIMENT/", "")))
