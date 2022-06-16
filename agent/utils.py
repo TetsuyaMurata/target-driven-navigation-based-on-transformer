92dff98c31438762ffff3938203de622076f3482
TetsuyaMurata <73677170+TetsuyaMurata@users.noreply.github.com>
2022-06-16 17:58:26 +0900

    Add files via upload

--- agent/utils.py
+++ agent/utils.py
@@ -3,6 +3,7 @@ import os
 import re
 import math
 import GPUtil
+
 def find_restore_point(checkpoint_path, fail = True):
     checkpoint_path = os.path.abspath(checkpoint_path)
 
@@ -25,6 +26,34 @@ def find_restore_point(checkpoint_path, fail = True):
             if fail:
                 raise Exception('Restore point not found')
             else: return None
+        return (checkpoint_path, None)
+
+def find_restore_points(checkpoint_path, fail=True):
+    checkpoint_path = os.path.abspath(checkpoint_path)
+
+    # Find latest checkpoint
+    restore_point = None
+    if checkpoint_path.find('{checkpoint}') != -1:
+        files = os.listdir(os.path.dirname(checkpoint_path))
+        base_name = os.path.basename(checkpoint_path)
+        regex = re.escape(base_name).replace(
+            re.escape('{checkpoint}'), '(\d+)')
+        points = [(fname, int(match.group(1))) for (fname, match) in (
+            (fname, re.match(regex, fname),) for fname in files) if not match is None]
+        if len(points) == 0:
+            if fail:
+                raise Exception('Restore point not found')
+            else:
+                return None
+        points = sorted(points, key=lambda x: x[1], reverse=True)
+        (base_name, restore_point) = zip(*points)
+        return (base_name, restore_point)
+    else:
+        if not os.path.exists(checkpoint_path):
+            if fail:
+                raise Exception('Restore point not found')
+            else:
+                return None
         return (checkpoint_path, None)
 
 def get_first_free_gpu(memory_needed):
